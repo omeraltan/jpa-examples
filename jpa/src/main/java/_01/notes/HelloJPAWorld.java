bb08a72438574726420c67b5d04b438b15d5e31a
omeraltan <omeraltan66@gmail.com>
2024-03-26 23:17:51 +0300

    Commit All

---
+++ jpa/src/main/java/_01/notes/HelloJPAWorld.java
@@ -0,0 +1,10 @@
+package _01.notes;
+
+public class HelloJPAWorld {
+    public static void main(String[] args) {
+
+    }
+}
+
+// ORM -> Object Relational Mapping
+// ORM -> Java objeleri ile ilişkisel veritabanı arasındaki bağlantıyı yönetimi sağlayan tekniktir.
